6f918dc58f7f63dbcbeddc49cb953de778bf0c10
breannams <breannamarie_s@hotmail.com>
2022-01-10 16:15:29 -0500

    Add canvas and frame for styling.

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ import os
 
 root = tk.Tk()
 
+canvas = tk.Canvas(root, height = 700, width = 700, bg = "teal")
+canvas.pack()
+
+frame = tk.Frame(root, bg="pink")
+frame.place(relwidth = 0.8, relheight = 0.8, relx=0.1,rely=0.1)
+
+
 root.mainloop()
 
