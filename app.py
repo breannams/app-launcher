2cc090a7ff313b88c494a215c23709c900660d57
breannams <breannamarie_s@hotmail.com>
2022-01-10 16:53:10 -0500

    Git rid of duplicate & unnecessary code.

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 root = tk.Tk()
 apps = []
 
-#functions
 
 if os.path.isfile('save.txt'):
     with open('save.txt', 'r') as f:
@@ -29,14 +28,14 @@ def runApps():
     for app in apps:
         os.startfile(app)
 
-#canvas styling
+
 canvas = tk.Canvas(root, height=700, width=700, bg="teal")
 canvas.pack()
 
 frame = tk.Frame(root, bg="pink")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-#buttons
+
 openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="teal", command=addApp)
 openFile.pack()
 
@@ -48,19 +47,6 @@ for app in apps:
     label = tk.Label(frame, text=app, bg="gray", fg="white")
     label.pack()
 
-#canvas styling
-canvas = tk.Canvas(root, height=700, width=700, bg="teal")
-canvas.pack()
-
-frame = tk.Frame(root, bg="pink")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-#buttons
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="teal", command=addApp)
-openFile.pack()
-
-runAllApps = tk.Button(root, text="Run your Apps", padx=10, pady=5, fg="white", bg="teal", command=runApps)
-runAllApps.pack()
 root.mainloop()
 
 with open('save.txt', 'w') as f:
