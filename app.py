eefca112c769cc3b25d7274c9dfa99ea9d9087e5
breannams <breannamarie_s@hotmail.com>
2022-01-10 16:51:27 -0500

    Add ability to save files to frame after exiting app.

--- app.py
+++ app.py
@@ -44,7 +44,26 @@ runAllApps = tk.Button(root, text="Run your Apps", padx=10, pady=5, fg="white", 
 runAllApps.pack()
 
 
+for app in apps:
+    label = tk.Label(frame, text=app, bg="gray", fg="white")
+    label.pack()
+
+#canvas styling
+canvas = tk.Canvas(root, height=700, width=700, bg="teal")
+canvas.pack()
+
+frame = tk.Frame(root, bg="pink")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+#buttons
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="teal", command=addApp)
+openFile.pack()
+
+runAllApps = tk.Button(root, text="Run your Apps", padx=10, pady=5, fg="white", bg="teal", command=runApps)
+runAllApps.pack()
 root.mainloop()
 
-
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ",")
 
