a38b6be180292d5fe44626d1c091754d25d15fc9
breannams <breannamarie_s@hotmail.com>
2022-01-10 16:17:14 -0500

    Create buttons.

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ canvas.pack()
 frame = tk.Frame(root, bg="pink")
 frame.place(relwidth = 0.8, relheight = 0.8, relx=0.1,rely=0.1)
 
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="teal")
+openFile.pack()
+
+runAllApps = tk.Button(root, text="Run your Apps", padx=10, pady=5, fg="white", bg="teal")
+runAllApps.pack()
 
 root.mainloop()
 
